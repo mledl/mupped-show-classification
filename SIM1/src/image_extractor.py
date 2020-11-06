6b68d6aeff8aa436c6b1be9d16c0aba1cf07b4bd
Martin Ledl <martin.ledl55@gmail.com>
2020-11-06 19:27:42 +0100

    add ground truth extractor

---
+++ SIM1/src/image_extractor.py
@@ -0,0 +1,33 @@
+import cv2
+from pathlib import Path
+
+
+def extract_ground_truth_images(ground_truth_textfile, video_path, image_path):
+    cap = cv2.VideoCapture(video_path)
+    Path(image_path).mkdir(parents=True, exist_ok=True)
+
+    i = 0
+    with open(ground_truth_textfile, 'r') as f:
+        for line in f:
+            entries = line.split(', ')
+            cap.set(cv2.CAP_PROP_POS_FRAMES, int(entries[0]))
+            ret, frame = cap.read()
+            if not ret:
+                print('Failed to read frame %d of video %r.', frame_id, movie_path)
+                exit(1)
+
+            filename = image_path + '/' + str(entries[0]) + '_' + \
+                       '_'.join([str(label).rstrip() for label in entries[1:]]) + '.png'
+            cv2.imwrite(filename, frame)
+            if i == 200:
+                break
+            i += 1
+
+
+if __name__ == '__main__':
+    test_video_path = '../../videos/Muppets-02-01-01.avi'
+    test_ground_truth_textfile = '../../ground_truth/Muppets-02-01-01/Muppets-02-01-01.txt'
+    test_image_path = '../../ground_truth/labeled'
+
+    extract_ground_truth_images(ground_truth_textfile=test_ground_truth_textfile, video_path=test_video_path,
+                                image_path=test_image_path)
