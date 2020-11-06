000f05d5800621d652fa864504e36d449a3480ec
Martin Ledl <martin.ledl55@gmail.com>
2020-11-06 16:19:44 +0100

    add save labeled images and grount truth txt for Muppets-02-01-01

--- SIM1/src/labeler.py
+++ SIM1/src/labeler.py
@@ -1,6 +1,7 @@
 import cv2
 import matplotlib.pyplot as plt
 import os
+from pathlib import Path
 
 character_map = {0: 'kermit_the_frog',
                  1: 'waldorf_and_statler',
@@ -9,9 +10,10 @@ character_map = {0: 'kermit_the_frog',
                  4: 'none'}
 
 
-def labelize_data(movie_path, output_path, step_size=1):
+def labelize_data(movie_path, output_path, image_path, step_size=1):
     cap = cv2.VideoCapture(movie_path)
     last_frame_id = 1
+    Path(image_path).mkdir(parents=True, exist_ok=True)
 
     # if there is already a labeled output file, resume labeling
     if os.path.isfile(output_path):
@@ -35,7 +37,8 @@ def labelize_data(movie_path, output_path, step_size=1):
         output_file.close()
         exit(1)
 
-    output_line = str(frame_id)
+    output_line = str(int(frame_id))
+    image_filename = image_path + str(int(frame_id))
 
     f = plt.ion()
     im = plt.imshow(frame)
@@ -47,17 +50,23 @@ def labelize_data(movie_path, output_path, step_size=1):
         exit(1)
     if 'n' in label_key.lower():
         output_file.write(str(frame_id) + ', 4\n')
+        cv2.imwrite(image_filename + '_4' + '.png', cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
     if 'k' in label_key.lower():
         output_line = output_line + ', 0'
+        image_filename = image_filename + '_0'
     if 'w' in label_key.lower():
         output_line = output_line + ', 1'
+        image_filename = image_filename + '_1'
     if 'p' in label_key.lower():
         output_line = output_line + ', 2'
+        image_filename = image_filename + '_2'
     if 's' in label_key.lower():
         output_line = output_line + ', 3'
+        image_filename = image_filename + '_3'
 
     if 'n' not in label_key.lower():
         output_file.write(output_line + '\n')
+        cv2.imwrite(image_filename + '.png', cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
 
     while True:
         try:
@@ -70,6 +79,7 @@ def labelize_data(movie_path, output_path, step_size=1):
                 break
 
             output_line = str(int(frame_id))
+            image_filename = image_path + str(int(frame_id))
             im.set_data(frame)
             plt.draw()
             label_key = input('[k] kermit, [w] waldorf & statler, [p] pig, [s] swedish chef, [n] none: ')
@@ -78,18 +88,24 @@ def labelize_data(movie_path, output_path, step_size=1):
                 break
             if 'n' in label_key.lower():
                 output_file.write(str(frame_id) + ', 4\n')
+                cv2.imwrite(image_filename + '_4' + '.png', cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
                 i = i + step_size
                 continue
             if 'k' in label_key.lower():
                 output_line = output_line + ', 0'
+                image_filename = image_filename + '_0'
             if 'w' in label_key.lower():
                 output_line = output_line + ', 1'
+                image_filename = image_filename + '_1'
             if 'p' in label_key.lower():
                 output_line = output_line + ', 2'
+                image_filename = image_filename + '_2'
             if 's' in label_key.lower():
                 output_line = output_line + ', 3'
+                image_filename = image_filename + '_3'
 
             output_file.write(output_line + '\n')
+            cv2.imwrite(image_filename + '.png', cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
             i = i + step_size
         except KeyboardInterrupt:
             break
@@ -100,6 +116,7 @@ def labelize_data(movie_path, output_path, step_size=1):
 
 if __name__ == '__main__':
     test_movie_path = '../../videos/Muppets-02-01-01.avi'
-    test_ouput_path = '../../ground_truth/Muppets-02-01-01.txt'
+    test_ouput_path = '../../ground_truth/labeled/Muppets-02-01-01.txt'
+    test_image_path = '../../ground_truth/labeled/'
     step = 12
-    labelize_data(movie_path=test_movie_path, output_path=test_ouput_path, step_size=step)
+    labelize_data(movie_path=test_movie_path, output_path=test_ouput_path, image_path=test_image_path, step_size=step)
